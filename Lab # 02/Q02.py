4c30123c68409290973e443e36c67ae37e05eb50
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-08-31 13:01:15 +0500

    Create Q02.py

---
+++ Lab # 02/Q02.py
@@ -0,0 +1,9 @@
+str = input("write a string : ")
+
+lastCh = str[-1]
+
+if(lastCh == 'a' or lastCh == 'e' or lastCh == 'i' or lastCh == 'o' or lastCh == 'u' or lastCh == 'A' or lastCh == 'E' or lastCh == 'I' or lastCh == 'O' or lastCh == 'U' ):
+    print("The last character of the string is vowel")
+else:
+    print("The last character of the string is consonent")
+    
