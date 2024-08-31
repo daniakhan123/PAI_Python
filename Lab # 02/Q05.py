5fde8265ebb558c84935b4cb1633cac99c23eea9
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-08-31 14:33:38 +0500

    Update Q05.py

--- Lab # 02/Q05.py
+++ Lab # 02/Q05.py
@@ -1,1 +1,14 @@
+def fact(n):
+    if(n == 0 or n== 1):
+        print(1)
+    elif(n>1):
+        count = n
+        while(count > 1):
+            n = n * (count-1)
+            count = count -1
+        print("Facrorial is : ",n)  
+    
+    return 
 
+n = int(input("Enter number : "))
+fact(n)
