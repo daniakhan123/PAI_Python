c4db0d4285f51b618d2757bc5522ef299595ead6
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-08-31 19:35:33 +0500

    Update Q06.py

--- Lab # 02/Q06.py
+++ Lab # 02/Q06.py
@@ -1,1 +1,17 @@
+list1  = input("Enter list : ")
+list2 = [int(x) for x in list1.split()]#make this step easier please
 
+
+print(list2)
+
+mul = 1
+
+for i in range(0 , len(list2)):
+    mul = mul * i
+
+print("product of all elements in the list is : " , mul)
+
+    
+    
+    
+    
