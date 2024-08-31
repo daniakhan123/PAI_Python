fad6d8574eafffb951ff238c448be62455b9d907
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-08-31 14:01:25 +0500

    Update Q04.py

--- Lab # 02/Q04.py
+++ Lab # 02/Q04.py
@@ -1,1 +1,20 @@
+def employee(name, sal=None):
+    if sal is None:
+        sal = 10000
 
+    NewSalary= sal - (sal * 0.02)
+    
+    print("Employee Name:", name)
+    print("Employee Salary after Deduction of 2% tax:", NewSalary)
+    return
+
+name = input("Employee name: ")
+salary = input("Salary (leave blank for default): ")
+
+
+if salary != " ":
+    sal = float(salary)
+else:
+    sal = None
+
+employee(name, sal)
