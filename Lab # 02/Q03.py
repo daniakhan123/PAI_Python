241bc0e2f924f5b349375afdc25c62baf629a415
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-08-31 13:37:07 +0500

    Update Q03.py

--- Lab # 02/Q03.py
+++ Lab # 02/Q03.py
@@ -1,1 +1,9 @@
+user = input("Enter URL : ")
 
+print("original URl : " ,user)
+
+if user.startswith("http://www."):
+    user2 = user.removeprefix("http://www.")
+    new = user2 + '.com'
+    print("New URL : " , new)
+    
