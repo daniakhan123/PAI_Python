23b6afde2efa07746b58ee2972205f08154b45c3
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-09-04 23:54:31 +0500

    Update Q12.py

--- Lab # 02/Q12.py
+++ Lab # 02/Q12.py
@@ -1,1 +1,21 @@
+keys = []
+values = []
 
+num = int(input("Enter the number of elements you want to add to the lists: "))
+
+
+print("Enter elements for list 1 :")
+for i in range(num):
+    element = input("Enter element  for list 1: ")
+    keys.append(element)
+
+print("Enter elements for list 2 :")
+for i in range(num):
+    element2 = input("Enter element for list 2: ")
+    values.append(element2)
+
+dict = {}
+for i in range(num):
+    dict[keys[i]] = values[i]
+
+print(dict)
