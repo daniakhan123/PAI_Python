c6e23b6c8f50b8560bda14b524d584c20cdcc41d
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-09-04 23:03:56 +0500

    Update Q10.py

--- Lab # 02/Q10.py
+++ Lab # 02/Q10.py
@@ -1,1 +1,12 @@
 
+def build_message(**info):
+    message = "Details provided:\n"
+    for key, value in info.items():
+        message += f"{key.capitalize()}: {value}\n"
+    return message
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+city = input("Enter city: ")
+
+print(build_message(name=name, age=age, city=city))
