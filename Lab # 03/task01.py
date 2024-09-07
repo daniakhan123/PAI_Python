410d5ebc5aa1ad08bdc8e9ea99a2c61e21a0a55a
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-09-07 23:41:11 +0500

    Update task01.py

--- Lab # 03/task01.py
+++ Lab # 03/task01.py
@@ -1,1 +1,14 @@
+try:
+    with open('text1.txt', 'r') as file:
+        content = file.read()
 
+    num_characters = len(content)
+  
+    print(f"Total number of characters: {num_characters}")
+
+except FileNotFoundError:
+    print("Error: The file  was not found.")
+except IOError:
+    print("Error: There was an issue reading the file.")
+except Exception as e:
+    print("An unexpected error occurred")
