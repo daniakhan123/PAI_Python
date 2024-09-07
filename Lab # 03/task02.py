3e0cb9f27167da3861e0c9ae63493d90b0080d2a
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-09-07 23:44:08 +0500

    Update task02.py

--- Lab # 03/task02.py
+++ Lab # 03/task02.py
@@ -1,1 +1,23 @@
+def replaceWord(filename, wordfind, repword):
+    try:
+        with open('text.txt', 'r') as file:
+            content = file.read()
+        
+        newContent= content.replace(wordfind, repword)
+        
+        with open(filename, 'w') as file:
+            file.write(newContent)
+        
+        print("All occurrences have been replaced")
+    
+    except FileNotFoundError:
+        print("Error: The file  was not found.")
+    except IOError:
+        print("Error: There was an issue reading or writing to the file.")
+    except Exception as e:
+        print("An unexpected error occurred")
 
+
+wordfind = 'python'
+repword = 'Cpp'
+replaceWord('text.txt', wordfind, repword)
