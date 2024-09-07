35e492ec3471af51cea760126dbe2c6c5ec9bf89
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-09-08 02:07:59 +0500

    Update task06.py

--- Lab # 03/task06.py
+++ Lab # 03/task06.py
@@ -1,1 +1,14 @@
-
+def write_sentence():
+    sentence = input("Enter any sentence : ")
+    if sentence.endswith("?"):
+        with open("questions.txt" , "a") as file:
+            file.write(sentence + "\n")
+            print("The question has been wrtitten")
+try:
+    write_sentence()
+except FileNotFoundError:
+    print("Error: The file  was not found.")
+except IOError:
+    print("Error: There was an issue reading the file.")
+except Exception as e:
+    print("An unexpected error occurred")
