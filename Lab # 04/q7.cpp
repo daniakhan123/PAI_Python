406639c6e756ae7b559ecca15b15d001bf46f40a
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-09-16 22:01:15 +0500

    Create q7.cpp

---
+++ Lab # 04/q7.cpp
@@ -0,0 +1,18 @@
+class Vehicle:
+
+    capacity=20
+    fare=capacity *100
+
+    def Printdata(self):
+        print("Seating capacityacity: ", self.capacity)
+        print("Fare: ", self.fare)
+
+
+class Bus(Vehicle):
+    amount=Vehicle.fare+10/100
+    def Printdata(self):
+        print("Seating capacityacity: ", Vehicle.capacity)
+        print("Total Fare: ", self.amount)
+
+b1=Bus()
+b1.Printdata()
