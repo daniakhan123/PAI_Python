cde1820e24edc06296c7f9e1167f2d3644ee221c
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-10-01 00:30:41 +0500

    Update q01.py

--- Lab # 06/q01.py
+++ Lab # 06/q01.py
@@ -1,1 +1,12 @@
 
+import pandas as pd
+
+
+movie = {"Movie Name": ["Titanic", "DeadPool", "Avatar", "No Way Home", "Avengers Endgame"],
+    "Revenue": [2100000, 2900000, 1320000, 69000, 784900],
+    "Budget": [200000, 500000, 200000, 100000, 300000]}
+
+df = pd.DataFrame(movie)
+print_movies = df[(df["Revenue"] > 2000000) & (df["Budget"] < 1000000)]
+
+print(print_movies)
