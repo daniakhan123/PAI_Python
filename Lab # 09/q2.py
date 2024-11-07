1ca11197fc66e4b54d95cd85ca9951ef4edb42f3
daniakhan123 <142868029+daniakhan123@users.noreply.github.com>
2024-11-08 03:35:58 +0500

    Update q2.py

--- Lab # 09/q2.py
+++ Lab # 09/q2.py
@@ -1,1 +1,11 @@
 
+import matplotlib.pyplot as plt
+
+cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+populations = [8175133, 3792621, 2695598, 2129784, 1445632]
+
+plt.barh(cities, populations, color='skyblue')
+plt.xlabel("Population")
+plt.ylabel("Cities")
+plt.title("Population of Cities")
+plt.show()
